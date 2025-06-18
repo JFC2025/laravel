b6e4972275ab7c08c51375d9ede2c54c4b9a8ff5
JFC2025 <jotaefecano@gmail.com>
2025-06-18 13:41:57 +0100

    operaciones arimeticas

--- index.php
+++ index.php
@@ -11,8 +11,22 @@
     <h1 style="color: blue;">Bienvenido a mi Web</h1>
     <?php
     echo '<span style="color: red;">Hola mundo!</span>';
-    echo '<br>'; // comentario en linea espacio
+   
     echo '<span style="color: black;">Hola Francis!</span>';
+
+    // variable en php
+    $num1 = 10;
+    $num2 = 2.5;
+
+    echo '<br>'; // comentario en linea espacio
+    
+    echo "numero 10 numero 2.5";
+    echo "<br>".'Suma:'.$num1+$num2; // concatenación de variables 
+
+    echo "<br>".'multiplica:'.$num1*$num2; // concatenación de variables 
+
+     echo "<br>".'division:'.$num1/$num2; // concatenación de variables
+
     ?>
 </body>
 
