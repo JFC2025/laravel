2f6ea90f407cadcdf49c4b1cbd889c3091b39b24
Anna <anna.vinas.sauch@gmail.com>
2025-06-18 11:53:09 +0100

    primer commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Web</title>
+</head>
+
+<body>
+    <h1 style="color: blue;">Bienvenido a mi Web</h1>
+    <?php
+    echo '<span style="color: red;">Hola mundo!</span>';
+    echo '<br>'; // comentario en linea
+    echo '<span style="color: black;">Hola Francis!</span>';
+    ?>
+</body>
+
+</html>
